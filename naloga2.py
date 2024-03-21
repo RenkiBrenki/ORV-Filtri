bcf9845a9831c5e9c19bd4e453bc37e9806e4510
Rene Jausovec <jausovec.rene@gmail.com>
2024-03-21 17:37:50 +0100

    Implemented image convolution

--- naloga2.py
+++ naloga2.py
@@ -1,28 +1,81 @@
 import cv2 as cv
 import numpy as np
 
-def konvolucija(slika, jedro):
+import numpy as np
+import cv2 as cv
+
+
+def konvolucija(image, kernel):
     '''Izvede konvolucijo nad sliko. Brez uporabe funkcije cv.filter2D, ali katerekoli druge funkcije, ki izvaja konvolucijo.
     Funkcijo implementirajte sami z uporabo zank oz. vektorskega računanja.'''
-    pass
+    kernel_height, kernel_width = kernel.shape
+    #image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-def filtriraj_z_gaussovim_jedrom(slika,sigma):
+    padding_height = kernel_height // 2
+    padding_width = kernel_width // 2
+
+    padded_image = np.pad(image, ((padding_height, padding_height), (padding_width, padding_width), (0, 0)), mode='constant')
+
+    image_height, image_width, channels = image.shape
+    return_image = np.zeros_like(image)
+
+    for c in range(channels):
+        for i in range(image_height):
+            for j in range(image_width):
+                # Extract the image patch
+                image_patch = padded_image[i:i + kernel_height, j:j + kernel_width, c]
+
+                # Compute the convolution
+                result = np.sum(image_patch * kernel)
+
+                # Save the result to the corresponding pixel in the output image
+                return_image[i, j, c] = result
+
+    return return_image[:image_height, :image_width]
+
+def filtriraj_z_gaussovim_jedrom(slika, sigma=float):
     '''Filtrira sliko z Gaussovim jedrom..'''
     pass
 
 def filtriraj_sobel_smer(slika):
     '''Filtrira sliko z Sobelovim jedrom in označi gradiente v orignalni sliki glede na ustrezen pogoj.'''
+
     pass
+
+def zmanjsaj_sliko(slika, sirina, visina):
+    '''Zmanjšaj sliko na velikost sirina x visina.'''
+    return cv.resize(slika, (sirina, visina))
 
 if __name__ == '__main__':
-    camera = cv.VideoCapture(1)
-    while True:
-        ret, frame = camera.read()
-        cv.imshow("Camera", frame)
 
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+    image = cv.imread(".utils/lenna.png")
+    kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32)/9
+    kernel2 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32) #Corner detection
 
-    camera.release()
+    cv.imshow("original", image)
+
+    image = filtriraj_z_gaussovim_jedrom(image, 1.0)
+
+    cv.imshow("Gaussian blur", image)
+
+    cv.waitKey(0)
     cv.destroyAllWindows()
-    pass
+
+    #camera = cv.VideoCapture(1)
+    # while True:
+    #     ret, frame = camera.read()
+    #
+    #     #frame = zmanjsaj_sliko(frame, 300, 260)
+    #
+    #     kernel1 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32)  #Box blur
+    #     #kernel2 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32) #Corner detection
+    #
+    #     frame = konvolucija(frame, kernel1)
+    #
+    #     cv.imshow("Camera", frame)
+    #
+    #     if cv.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    #
+    # camera.release()
+    # cv.destroyAllWindows()
